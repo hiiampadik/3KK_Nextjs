3dff34002df4f9318861fbb2b42ccc4535a0449a
Bronislav Musil <brona.musil@gmail.com>
2025-11-17 16:39:24 +0100

    Nav draft

--- components/Layout/Navigation.tsx
+++ components/Layout/Navigation.tsx
@@ -9,7 +9,7 @@ import styles from './navigation.module.scss';
 import {classNames} from '@/components/utils/classNames';
 import {useTranslations} from 'next-intl';
 import {useLocale} from '@/components/utils/useLocale';
-import logo from '@/public/logos/2.svg';
+import logo from '@/public/logos/3.svg';
 import Figure from '@/components/Sanity/Figure';
 import BlockContent from '@/components/Sanity/BlockContent';
 import {LocalizedRichParagraph} from '@/api/sanity.types';
@@ -27,6 +27,8 @@ const Navigation: FunctionComponent<Props> = ({cover, description}) => {
     const t = useTranslations('Navigation');
 
     const [showOverlay, setShowOverlay] = useState(false);
+    const [navVisible, setNavVisible] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
 
     useEffect(() => {
         const handleRouteChange = () => {
@@ -36,77 +38,96 @@ const Navigation: FunctionComponent<Props> = ({cover, description}) => {
         return () => router.events.off('routeChangeComplete', handleRouteChange);
     }, [router.events]);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            if (currentScrollY > lastScrollY) {
+                // Scrolling down
+                setNavVisible(false);
+            } else {
+                // Scrolling up
+                setNavVisible(true);
+            }
+
+            setLastScrollY(currentScrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll, { passive: true });
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [lastScrollY]);
+
     useDisableScroll(showOverlay)
 
     return (
         <>
-            <div className={classNames([styles.navWrapper, router.asPath === '/' && styles.animation, cover && styles.navWithCover])}>
-                <nav>
-                    <div className={styles.navLeft}>
-                        <Link href={"/"} className={classNames([styles.link, styles.logo])}>
-                            <img src={logo.src} className={styles.logoComplex}/>
-                        </Link>
-                        <Link
-                            href={"/projects"}
-                            className={classNames([styles.link, styles.link1])}
-                            prefetch={false}>
-                            <p>{t('projects')}</p>
-                        </Link>
-                        <Link
-                            href={"/about"}
-                            className={classNames([styles.link, styles.link2])}
-                            prefetch={false}
-                        >
-                            <p>{t('about')}</p>
-                        </Link>
-                        <Link
-                            href={"/contact"}
-                            className={classNames([styles.link, styles.link3])}
-                            prefetch={false}
-                        >
-                            <p>{t('contact')}</p>
-                        </Link>
-                    </div>
+            <nav className={classNames([styles.nav, navVisible ? styles.show : styles.hidden])}>
+                <div className={styles.navLeft}>
+                    <Link href={"/"} className={classNames([styles.logo])}>
+                        <img src={logo.src} className={styles.logoComplex}/>
+                    </Link>
+                    <Link
+                        href={"/projects"}
+                        className={classNames([styles.link, styles.link1])}
+                        prefetch={false}>
+                        <p>{t('projects')}</p>
+                    </Link>
+                    <Link
+                        href={"/about"}
+                        className={classNames([styles.link, styles.link2])}
+                        prefetch={false}
+                    >
+                        <p>{t('about')}</p>
+                    </Link>
+                    <Link
+                        href={"/contact"}
+                        className={classNames([styles.link, styles.link3])}
+                        prefetch={false}
+                    >
+                        <p>{t('contact')}</p>
+                    </Link>
+                </div>
 
-                    <div className={styles.navRight}>
-                        <Link
-                            href={router.asPath}
-                            locale={router.locale === "cs" ? "en" : "cs"}
-                            className={classNames([styles.smallLink, styles.language])}
-                            prefetch={false}
-                        >
-                            <p>{router.locale === "cs" ? "En" : "Cz"}</p>
-                        </Link>
-                        <a href={"https://connect.boomevents.org/cs/organizer/d2324245-882a-43e7-8e7d-577816aa2926"}
-                           className={classNames([styles.smallLink, styles.tickets])}>
-                            <p>{t('tickets')}</p>
-                        </a>
-                        <a href={"https://www.instagram.com/3pluskk/"}
-                           className={classNames([styles.smallLink, styles.socials])}>
-                            <p>IG</p>
-                        </a>
-                        <a href={"https://www.facebook.com/3pluskk"}
-                           className={classNames([styles.smallLink, styles.socials])}>
-                            <p>FB</p>
-                        </a>
-                    </div>
-                </nav>
+                <div className={styles.navRight}>
+                    <Link
+                        href={router.asPath}
+                        locale={router.locale === "cs" ? "en" : "cs"}
+                        className={classNames([styles.smallLink, styles.link4])}
+                        prefetch={false}
+                    >
+                        <p>{router.locale === "cs" ? "En" : "Cz"}</p>
+                    </Link>
+                    <a href={"https://connect.boomevents.org/cs/organizer/d2324245-882a-43e7-8e7d-577816aa2926"}
+                       className={classNames([styles.smallLink, styles.tickets, styles.link5])}>
+                        <p>{t('tickets')}</p>
+                    </a>
+                    <a href={"https://www.instagram.com/3pluskk/"}
+                       className={classNames([styles.smallLink, styles.link6])}>
+                        <p>IG</p>
+                    </a>
+                    <a href={"https://www.facebook.com/3pluskk"}
+                       className={classNames([styles.smallLink, styles.link7])}>
+                        <p>FB</p>
+                    </a>
+                </div>
+            </nav>
 
-                {cover &&
-                    <div className={styles.coverContainer}>
-                        <div className={styles.cover}>
-                            <Figure image={cover} fullWidth={true}/>
+            {cover ?
+                <div className={styles.coverContainer}>
+                    <div className={styles.cover}>
+                        <Figure image={cover} fullWidth={true}/>
+                    </div>
+                    {description &&
+                        <div className={styles.description}>
+                            <BlockContent blocks={description[locale]}/>
                         </div>
-                        {description &&
-                            <div className={styles.description}>
-                                <BlockContent blocks={description[locale]}/>
-                            </div>
-                        }
-                    </div>
-                }
-            </div>
+                    }
+                </div>
+                :
+                <div className={styles.spacer}/>
+            }
 
-            <Overlay handleClose={() => setShowOverlay(false)} isOpen={showOverlay} />
+            <Overlay handleClose={() => setShowOverlay(false)} isOpen={showOverlay}/>
         </>
     );
 };
