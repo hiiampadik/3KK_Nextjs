b86e84d09a0e1c01f405d1451da45a5652e64de3
Bronislav Musil <brona.musil@gmail.com>
2026-04-20 19:58:57 +0200

    GithubPages

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,36 @@
 import type {NextConfig} from 'next';
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
-    i18n: {
-        defaultLocale: 'cs',
-        locales: ['en', 'cs'],
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cdn.sanity.io',
-            },
-        ]
-    },
+    ...(isGithubPages && {
+        output: 'export',
+        basePath: '/3kk',
+        images: {
+            unoptimized: true,
+        },
+    }),
+    ...(!isGithubPages && {
+        images: {
+            remotePatterns: [
+                {
+                    protocol: 'https',
+                    hostname: 'cdn.sanity.io',
+                },
+            ],
+        },
+    }),
+    ...(!isGithubPages && {
+        i18n: {
+            defaultLocale: 'cs',
+            locales: ['en', 'cs'],
+        },
+    }),
     logging: {
         fetches: {
             fullUrl: true,
         },
     },
-
 };
 
 export default nextConfig;
