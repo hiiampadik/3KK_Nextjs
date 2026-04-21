77d492891c44010985720396e154c7ee1f69c30d
Bronislav Musil <brona.musil@gmail.com>
2026-04-21 14:36:05 +0200

    Sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || "https://www.tripluskk.com",
-  generateRobotsTxt: true,
-  // Sitemaps are generated dynamically from fetched Sanity content
-  sitemapSize: 0,
-  exclude: ["*"],
+  siteUrl: "https://www.tripluskk.com",
+  generateRobotsTxt: false,
+  outDir: "./out",
 };
 
 export default config
